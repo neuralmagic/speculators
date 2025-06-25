73bfd75c9c19990d9965e0a94254a93bb7c6d567
MeganEFlynn <mflynn@redhat.com>
2025-06-25 18:35:52 +0000

    example scripts for data gen

--- src/speculators/training/Eagle/TTT-HASS/gen_data.sh
+++ src/speculators/training/Eagle/TTT-HASS/gen_data.sh
@@ -4,6 +4,18 @@
 # to get the dataset, run: wget https://huggingface.co/datasets/Aeala/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V4.3_unfiltered_cleaned_split.json .
 
 python -m ge_data.allocation \
---outdir /tmpdata/megan/ultrachat_gen \
+--outdir dataDirectory/sharegpt \
 --data_path ShareGPT_V4.3_unfiltered_cleaned_split.json \
---model_path meta-llama/Llama-3.1-8B-Instruct
+--model_path meta-llama/Llama-3.1-8B-Instruct \
+--chat_template llama \
+--dataset sharegpt
+
+
+# python -m ge_data.allocation \
+# --outdir dataDirectory/sharegpt \
+# --data_path ShareGPT_V4.3_unfiltered_cleaned_split.json \
+# --model_path mistralai/Mistral-Small-3.1-24B-Instruct-2503 \
+# --chat_template mistral \
+# --dataset sharegpt
+
+
